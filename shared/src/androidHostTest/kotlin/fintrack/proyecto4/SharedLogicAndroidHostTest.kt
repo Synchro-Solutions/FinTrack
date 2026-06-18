3a4d9692ae36d7ba90df028ec482f115ea60bb41
lbarrantesm <lbarrantesm@ucenfotec.ac.cr>
2026-06-17 19:43:11 -0600

    Sube codigo inicial del proyecto Kotlin

---
+++ shared/src/androidHostTest/kotlin/fintrack/proyecto4/SharedLogicAndroidHostTest.kt
@@ -0,0 +1,12 @@
+package fintrack.proyecto4
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedLogicAndroidHostTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ shared/src/iosMain/kotlin/fintrack/proyecto4/MainViewController.kt
@@ -0,0 +1,5 @@
+package fintrack.proyecto4
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
